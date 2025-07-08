05c27d8f9bf688a68f4c4646c300a50d0cebdbfc
Thiago Henrique Alonso <thiagohenriquealonso@gmail.com>
2025-07-08 15:59:28 -0300

    Adicionando as aulas de arrays

--- Aula06EstruturaDeRepeticao04.java
+++ Aula06EstruturaDeRepeticao04.java
@@ -8,13 +8,9 @@ public class Aula06EstruturaDeRepeticao04 {
         for (int parcela = 1; parcela <= valorTotal; parcela++) {
             double valorParcela = valorTotal / parcela;
             if(valorParcela >= 1000){
-                System.out.println("Parcela " + parcela+ "R$"+valorParcela);
-            }else{
                 break;
-                for (int i = 0; i < ; i++) {
-                    
-                }
             }
+            System.out.println("Parcela" + parcela + "R$" + valorParcela);
         }
     }
 }
---
+++ Aula07Arrays01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.maratonajava;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        idades[3] = 18;
+        System.out.println(idades[0]);
+    }
+}
